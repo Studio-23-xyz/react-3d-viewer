33a32597379f1b68a6677a66b364d12c67ec917f
Shozon Raj <45089799+moshiuzzaman@users.noreply.github.com>
2021-09-14 12:01:13 +0600

    model url load frontend done

--- src/Components/Home/Controller/UploadModel.js
+++ src/Components/Home/Controller/UploadModel.js
@@ -1,13 +1,21 @@
-import React, { useContext } from 'react';
-import { Accordion, Form } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Accordion, ButtonGroup, Form, Button } from 'react-bootstrap';
 import { ControlData } from '../../../App';
 
 const UploadModel = () => {
 	const { setModel } = useContext(ControlData);
+	const [uploadType, setUploadType] = useState('file');
+	const [file, setFile] = useState();
+	const [url, setUrl] = useState();
 
 	const fileUpload = (e) => {
-		const url = URL.createObjectURL(e[0]);
-		setModel(url);
+		if (e[0]) {
+			const url = URL.createObjectURL(e[0]);
+			setModel(url);
+		}
+	};
+	const urlLoad = (e) => {
+		console.log(e);
 	};
 	return (
 		<Accordion.Item eventKey="4">
@@ -15,14 +23,44 @@ const UploadModel = () => {
 				Upload A model
 			</Accordion.Header>
 			<Accordion.Body>
+				<ButtonGroup className="mb-2 w-100">
+					<Button variant="info" onClick={() => setUploadType('file')}>
+						File Upload
+					</Button>
+					<Button onClick={() => setUploadType('url')}>Url Upload</Button>
+				</ButtonGroup>
 				<div className="upload_model">
-					<Form.Group controlId="formFile" className="mb-3">
-						<Form.Label>Upload a glb file</Form.Label>
-						<Form.Control
-							type="file"
-							onChange={(e) => fileUpload(e.target.files)}
-						/>
-					</Form.Group>
+					{uploadType === 'file' ? (
+						<Form.Group controlId="formFile" className="mb-3">
+							<Form.Label>Upload a gltf file</Form.Label>
+							<Form.Control
+								type="file"
+								onChange={(e) => setFile(e.target.files)}
+							/>
+							<Button
+								className="w-100 mt-3"
+								variant="success"
+								onClick={() => fileUpload(file)}
+							>
+								File Upload
+							</Button>
+						</Form.Group>
+					) : (
+						<Form.Group controlId="formFile" className="mb-3">
+							<Form.Label>Gltf model url</Form.Label>
+							<Form.Control
+								type="text"
+								onChange={(e) => setUrl(e.target.value)}
+							/>
+							<Button
+								className="w-100 mt-3"
+								variant="success"
+								onClick={() => urlLoad(url)}
+							>
+								Url Upload
+							</Button>
+						</Form.Group>
+					)}
 				</div>
 			</Accordion.Body>
 		</Accordion.Item>
